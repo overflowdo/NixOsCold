7855a4237fd92e5c0b688b98718743de3e66d50a
overflowdo <54685798+overflowdo@users.noreply.github.com>
2026-04-26 21:51:14 +0200

    bootable

--- Cold-Signer/configuration.nix
+++ Cold-Signer/configuration.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 
 {
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
   imports = [   
     ./profiles/hardware-vm.nix 
     ./hardware-configuration.nix
