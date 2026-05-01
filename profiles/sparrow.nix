e5ed404893c38cab74c6a3d24cbc4ebcc716f51d
overflowdo <54685798+overflowdo@users.noreply.github.com>
2026-05-01 20:19:17 +0200

    fdf

--- profiles/sparrow.nix
+++ profiles/sparrow.nix
@@ -1,73 +1,43 @@
 { config, pkgs, lib, ... }:
 
-let
-  sparrowPkg =
-    if pkgs ? sparrow-wallet then pkgs.sparrow-wallet
-    else if pkgs ? sparrow then pkgs.sparrow
-    else throw "No sparrow package found in this nixpkgs";
-
-  sparrowReal = lib.getExe sparrowPkg;
-
-  sparrowWrapped = pkgs.writeShellScriptBin "sparrow" ''
-    export GDK_BACKEND=x11
-
-    # Wichtig: nicht auf irgendeine systemweite Java-Installation zwingen
-    unset JAVA_HOME
-    unset CLASSPATH
-    unset _JAVA_OPTIONS
-    unset JAVA_TOOL_OPTIONS
-
-    # VM/Proxmox/ohne GPU: JavaFX stabiler ohne OpenGL/ES2
-    export _JAVA_OPTIONS="-Dprism.order=sw"
-
-    exec ${sparrowReal} "$@"
-  '';
-in
 {
+  # GSettings / dconf
   programs.dconf.enable = true;
 
+  # Flatpak aktivieren
+  services.flatpak.enable = true;
+
   environment.systemPackages = with pkgs; [
-    sparrowPkg
-    sparrowWrapped
-
-    # JavaFX/GTK Umfeld
-    glib
-    gtk3
-    gsettings-desktop-schemas
-
-    # Fonts + X11 Tools (häufig nötig)
-    fontconfig
-    dejavu_fonts
-    xorg.xrandr
-    xorg.xset
-
-    # X11 libs, die JavaFX gelegentlich braucht
-    xorg.libX11
-    xorg.libXext
-    xorg.libXi
-    xorg.libXrender
-    xorg.libXtst
-    xorg.libXxf86vm
+    flatpak
   ];
 
+  # XDG Portals → GTK (richtig für XFCE)
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = with pkgs; [
+    xdg-desktop-portal-gtk
+  ];
+
+  # Optional, aber sinnvoll in VMs
   environment.sessionVariables = {
     GDK_BACKEND = "x11";
   };
 
-  environment.etc."xdg/applications/sparrow.desktop" = {
+  # Desktop-Eintrag für Sparrow Flatpak
+  environment.etc."xdg/applications/sparrow-flatpak.desktop" = {
     mode = "0644";
     text = ''
       [Desktop Entry]
-      Name=Sparrow Wallet
-      Exec=${lib.getExe sparrowWrapped}
+      Name=Sparrow Wallet (Flatpak)
+      Exec=flatpak run com.sparrowwallet.Sparrow
       Type=Application
       Categories=Finance;
       Terminal=false
     '';
   };
 
+  # Desktop-Shortcut erzeugen
   systemd.tmpfiles.rules = lib.mkAfter [
     "d /home/user/Desktop 0755 user users - -"
-    "L+ /home/user/Desktop/sparrow.desktop - - - - /etc/xdg/applications/sparrow.desktop"
+    "L+ /home/user/Desktop/sparrow.desktop - - - - /etc/xdg/applications/sparrow-flatpak.desktop"
   ];
 }
